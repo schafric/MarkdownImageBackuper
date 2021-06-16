2b233245cdf98992262aec4fb3afda13ab8f6afa
schafric <richard.schafric@gmail.com>
2021-06-16 21:38:15 +0200

    initial c# console application template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkdownImageBackuper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
